33605fdd2966414ea65c748d44bb5963cf901482
Xizzz11 <davidnjg6@gmail.com>
2026-04-17 15:39:44 +0300

    Delete app directory

--- app/db/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import Base
-from .models.user import User
-from .models.task import Task
-
-
-__all__ = ["Base", "User", "Task"]
--- app/routers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .auth import router as auth_router
-from .tasks import router as tasks_router
-
-__all__ = ["auth_router", "tasks_router"]
